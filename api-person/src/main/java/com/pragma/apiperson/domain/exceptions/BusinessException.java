c04c160cda920ebc19d5d8adc870622d6158624a
LoperaUr <manuellopera612@gmail.com>
2026-06-17 11:57:10 -0500

    chore: add api-person as submodule

--- api-person/src/main/java/com/pragma/apiperson/domain/exceptions/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package com.pragma.apiperson.domain.exceptions;
-
-public class BusinessException extends RuntimeException {
-
-    public BusinessException(String message) {
-        super(message);
-    }
-}
